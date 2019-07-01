5204f023b42ee2fdc3c88a9fd18f3b9f55d4cb80
Rameez Parkar <rparkar@tavisca.in>
2019-07-01 15:44:27 +0530

    Updated language-basics-01

---
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/FixMultiplication.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
+{
+    class FixMultiplication
+    {
+        private static float value1, value2, answer;
+        private static string temp1 = null, temp2 = null;
+
+        public static int FindDigit(string equation)
+        {
+            string[] splitOnEquals = equation.Split('=');
+            string stringAnswer = splitOnEquals[1];
+            string[] splitOnMultiply = splitOnEquals[0].Split('*');
+            string stringValue1 = splitOnMultiply[0];
+            string stringValue2 = splitOnMultiply[1];
+
+            if (stringAnswer.Contains('?'))
+            {
+                CalculateCorrectAnswer(stringValue1, stringValue2, stringAnswer);
+            }
+            else if (stringValue1.Contains('?'))
+            {
+                CalculateCorrectValue1(stringValue1, stringValue2, stringAnswer);
+            }
+            else if (stringValue2.Contains('?'))
+            {
+                CalculateCorrectValue2(stringValue1, stringValue2, stringAnswer);
+            }
+
+            return FindMissingDigit();
+        }
+
+        private static void CalculateCorrectAnswer(string stringValue1, string stringValue2, string stringAnswer)
+        {
+            value1 = float.Parse(stringValue1);
+            value2 = float.Parse(stringValue2);
+            answer = value1 * value2;
+            temp1 = answer.ToString();
+            temp2 = stringAnswer;
+        }
+
+        private static void CalculateCorrectValue1(string stringValue1, string stringValue2, string stringAnswer)
+        {
+            value2 = float.Parse(stringValue2);
+            answer = float.Parse(stringAnswer);
+            value1 = answer / value2;
+            temp1 = value1.ToString();
+            temp2 = stringValue1;
+        }
+
+        private static void CalculateCorrectValue2(string stringValue1, string stringValue2, string stringAnswer)
+        {
+            value1 = float.Parse(stringValue1);
+            answer = float.Parse(stringAnswer);
+            value2 = answer / value1;
+            temp1 = value2.ToString();
+            temp2 = stringValue2;
+        }
+
+        private static int FindMissingDigit()
+        {
+            if (temp1.Length == temp2.Length)
+            {
+                return int.Parse(temp1[temp2.IndexOf('?')].ToString());
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
-    class FixMultiplication
+    class Program
     {
         static void Main(string[] args)
         {
@@ -16,60 +16,8 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         private static void Test(string args, int expected)
         {
-            var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
+            var result = FixMultiplication.FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
             Console.WriteLine($"{args} : {result}");
-        }
-
-        public static int FindDigit(string equation)
-        {
-            // Add your code here.
-            float val1, val2, answer;
-            string str_val1, str_val2, str_answer, temp1=null, temp2=null;
-            string[] split_on_equals = equation.Split('=');
-            str_answer = split_on_equals[1];
-            string[] split_on_multiply = split_on_equals[0].Split('*');
-            str_val1 = split_on_multiply[0];
-            str_val2 = split_on_multiply[1];
-            if(str_answer.Contains('?'))
-            {
-                val1 = float.Parse(split_on_multiply[0]);
-                val2 = float.Parse(split_on_multiply[1]);
-                answer = val1 * val2;
-                temp1 = answer.ToString();
-                temp2 = str_answer;
-            }
-            else if(str_val1.Contains('?'))
-            {
-                val2 = float.Parse(split_on_multiply[1]);
-                answer = float.Parse(split_on_equals[1]);
-                val1 = answer / val2;
-                temp1 = val1.ToString();
-                temp2 = str_val1;
-            }
-            else if(str_val2.Contains('?'))
-            {
-                val1 = float.Parse(split_on_multiply[0]);
-                answer = float.Parse(split_on_equals[1]);
-                val2 = answer / val1;
-                temp1 = val2.ToString();
-                temp2 = str_val2;
-            }
-
-            if(temp1.Length == temp2.Length)
-            {
-                for (int i = 0; i < temp1.Length; i++)
-                {
-                    if (temp1[i] != temp2[i])
-                    {
-                        return int.Parse(temp1[i].ToString());
-                    }
-                }
-            }
-            else
-            {
-                return -1;
-            }
-            throw new NotImplementedException();
         }
     }
 }
